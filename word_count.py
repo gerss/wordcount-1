217db041beb6e4ab0e6cf64ecd250a979c05fbec
gerss <gersson.fuenzalida@alumnos.usm.cl>
2017-01-05 11:22:23 -0300

    Use function

--- word_count.py
+++ word_count.py
@@ -1,10 +1,12 @@
-happy = input("Enter a statement to word count: ")
+	def wordcount(happy):
+		words = happy.split()
+		counts = {}
+		for word in words:
+		    counts[word] = counts.get(word, 0) + 1
 
-words = happy.split()
+		print("The word frequency of your statement is: ")
+		print(counts)
 
-counts = {}
-for word in words:
-    counts[word] = counts.get(word, 0) + 1
-
-print("The word frequency of your statement is: ")
-print(counts)
+	def main():
+		happy = input('Enter a statement to word count: ')
+		wordcount(happy)
