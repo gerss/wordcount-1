34eef24e6404280b1ff6f0a6dfe0f391a08ccfab
gerss <gersson.fuenzalida@alumnos.usm.cl>
2017-01-05 11:28:30 -0300

    add main

--- word_count.py
+++ word_count.py
@@ -1,12 +1,13 @@
-	def wordcount(happy):
-		words = happy.split()
-		counts = {}
-		for word in words:
-		    counts[word] = counts.get(word, 0) + 1
+def wordcount(happy):
+	words = happy.split()
+	counts = {}
+	for word in words:
+	    counts[word] = counts.get(word, 0) + 1
 
-		print("The word frequency of your statement is: ")
-		print(counts)
+	print("The word frequency of your statement is: ")
+	print(counts)
 
-	def main():
-		happy = input('Enter a statement to word count: ')
-		wordcount(happy)
+def main():
+	happy = input('Enter a statement to word count: ')
+	wordcount(happy)
+main()
